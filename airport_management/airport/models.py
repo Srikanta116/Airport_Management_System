9b349df81c91d0ebdae865b093bbdd855b709860
Srikanta Panigrahi <panigrahisrikanta11@gmail.com>
2024-03-18 09:25:51 +0530

    updated model

--- airport_management/airport/models.py
+++ airport_management/airport/models.py
@@ -92,7 +92,7 @@ class Role(models.Model):
     id = models.PositiveSmallIntegerField(
         choices=ROLE_CHOICES, primary_key=True)
     name = models.CharField(
-        max_length=100, choices=ROLE_CHOICES, blank=True, null=True)
+        max_length=100, blank=True, null=True)
 
     def __str__(self):
         return str(self.name)
